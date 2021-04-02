57930965951b3af46aee7d406b74d5629496c196
harshit50sit <harshitverma26@gmail.com>
2021-04-02 20:59:05 +0530

    Fundamental-2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict'; //because use to secure the js code and create visible error.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("I can drive :D")
+
+//const interface = 'Audio';
+//const private = 534;
+
+//Functions---
